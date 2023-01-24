2d20c85c257abab79487ee12e027a4698a56f932
ncclapyohands <nathancook648@gmail.com>
2023-01-24 09:14:15 -0700

    swagger

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+// const swagger = require('swagger-autogen');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 const router = express.Router();
 
 router.use('/contacts', require('./contacts'));
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
